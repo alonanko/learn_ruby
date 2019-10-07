d2c261cb86770e365987cadb822aba38d61e454f
alon <alon.ankonina@gmail.com>
2019-10-07 12:49:31 -0700

    finished book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
-# write your code here
+    def title
+        @title
+    end
+
+    def title=(title)
+        exceptions = ['a', 'an', 'and', 'the', 'in', 'of']
+
+        @title = title.split.map.with_index do |word, index| 
+            if !exceptions.include? word or index == 0
+                word.capitalize
+            else
+                word
+            end
+        end.join ' '
+
+    end
+
 end
